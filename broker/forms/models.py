c8f57701d1a3d6783693b0cca3bd60326b25e5b7
Lorenzo Tan <lorenzo.tan@gmail.com>
2019-01-07 03:55:48 -0800

    4: extending default user model

--- broker/forms/models.py
+++ broker/forms/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser
 
 #NOTE: Meta options: https://docs.djangoproject.com/en/dev/ref/models/options/
 
@@ -47,6 +48,7 @@ class NeedsList(models.Model):
 
 
 class PointOfContact(models.Model):
+    user       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50, default=None, null=True, blank=True)
     last_name  = models.CharField(max_length=50, default=None, null=True, blank=True)
     company    = models.CharField(max_length=100, default=None, null=True, blank=True)
@@ -56,6 +58,7 @@ class PointOfContact(models.Model):
 
 
 class ClientBusinessInfo(models.Model):
+    user         = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     occupation   = models.CharField(max_length=50, default=None, null=True, blank=True)
     company_name = models.CharField(max_length=50, default=None, null=True, blank=True)
     address      = models.CharField(max_length=50, default=None, null=True, blank=True)
@@ -68,6 +71,7 @@ class ClientLoanInfo(models.Model):
     # NOTE for currency
     # https://stackoverflow.com/questions/1139393/what-is-the-best-django-model-field-to-use-to-represent-a-us-dollar-amount
     # https://github.com/django-money/django-money
+    user     = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     amount   = models.DecimalField(max_digits=17, decimal_places=2)
     loan2val = models.DecimalField(max_digits=17, decimal_places=2)
     dscr     = models.DecimalField(max_digits=17, decimal_places=2)
@@ -75,6 +79,7 @@ class ClientLoanInfo(models.Model):
 
 
 class ClientFinancialInfo(models.Model):
+    user          = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     salary        = models.DecimalField(max_digits=17, decimal_places=2)
     yrs_in_biz    = models.PositiveSmallIntegerField()
     debt          = models.DecimalField(max_digits=17, decimal_places=2)
@@ -86,43 +91,40 @@ class ClientFinancialInfo(models.Model):
 
 
 class ClientPropertyInfo(models.Model):
+    user    = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     address = models.CharField(max_length=50, default=None, null=True, blank=True)
     value   = models.DecimalField(max_digits=17, decimal_places=2)
 
 
-class Clients(models.Model):
-    first_name = models.CharField(max_length=50, default=None, null=True, blank=True)
-    last_name  = models.CharField(max_length=50, default=None, null=True, blank=True)
-    address    = models.CharField(max_length=50, default=None, null=True, blank=True)
-    city       = models.CharField(max_length=50, default=None, null=True, blank=True)
-    state      = models.CharField(max_length=2, default=None, null=True, blank=True)
-    zip_code   = models.CharField(max_length=10, default=None, null=True, blank=True)
-    h_email    = models.EmailField(max_length=100, default=None, null=True, blank=True)
-    w_email    = models.EmailField(max_length=100, default=None, null=True, blank=True)
-    # NOTE https://github.com/VeryApt/django-phone-field
-    # https://stackoverflow.com/questions/19130942/whats-the-best-way-to-store-phone-number-in-django-models
-    h_phone    = models.CharField(max_length=10, blank=True)
-    w_phone    = models.CharField(max_length=10, blank=True)
-    loantype   = models.ForeignKey(LoanTypes, on_delete=models.SET_NULL, blank=True, null=True)
-    poc        = models.ManyToManyField(PointOfContact)
-    business   = models.ForeignKey(ClientBusinessInfo, on_delete=models.SET_NULL, blank=True, null=True)
-    qualifers  = models.ManyToManyField(Qualifiers)
-    needslist  = models.ManyToManyField(NeedsList)
+class Client(models.Model):
+    user      = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    loantype  = models.ForeignKey(LoanTypes, on_delete=models.SET_NULL, blank=True, null=True)
+    poc       = models.ManyToManyField(PointOfContact)
+    business  = models.ForeignKey(ClientBusinessInfo, on_delete=models.SET_NULL, blank=True, null=True)
+    qualifers = models.ManyToManyField(Qualifiers)
+    needslist = models.ManyToManyField(NeedsList)
 
 
-class Lenders(models.Model):
-    first_name    = models.CharField(max_length=50, default=None, null=True, blank=True)
-    last_name     = models.CharField(max_length=50, default=None, null=True, blank=True)
+class Lender(models.Model):
+    user          = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     company       = models.CharField(max_length=100, default=None, null=True, blank=True)
-    address       = models.CharField(max_length=50, default=None, null=True, blank=True)
-    city          = models.CharField(max_length=50, default=None, null=True, blank=True)
-    state         = models.CharField(max_length=2, default=None, null=True, blank=True)
-    zip_code      = models.CharField(max_length=10, default=None, null=True, blank=True)
-    h_email       = models.EmailField(max_length=100, default=None, null=True, blank=True)
-    w_email       = models.EmailField(max_length=100, default=None, null=True, blank=True)
-    h_phone       = models.CharField(max_length=10, blank=True)
-    w_phone       = models.CharField(max_length=10, blank=True)
     qualifers     = models.ManyToManyField(Qualifiers)
     propertytypes = models.ManyToManyField(PropertyTypes)
 
 
+class Broker(models.Model):
+    user     = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+# NOTE: https://docs.djangoproject.com/en/2.0/topics/auth/customizing/#extending-the-existing-user-model
+class User(AbstractUser):
+    user     = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    address  = models.CharField(max_length=50, default=None, null=True, blank=True)
+    city     = models.CharField(max_length=50, default=None, null=True, blank=True)
+    state    = models.CharField(max_length=2, default=None, null=True, blank=True)
+    zip_code = models.CharField(max_length=10, default=None, null=True, blank=True)
+    # NOTE https://github.com/VeryApt/django-phone-field
+    # https://stackoverflow.com/questions/19130942/whats-the-best-way-to-store-phone-number-in-django-models
+    phone    = models.CharField(max_length=10, blank=True)
+
+    def __str__(self):
+        return self.email
