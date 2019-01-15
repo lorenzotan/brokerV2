3567fc1057fc57b1006ea043275651032f330d99
Lorenzo Tan <lorenzo.tan@gmail.com>
2019-01-15 05:16:19 -0800

    adding placeholder for query logging

--- broker/broker/settings.py
+++ broker/broker/settings.py
@@ -104,6 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# how to log queries
+#LOGGING = {
+#    'loggers': {
+#        'django.db.backends': {
+#            'level': 'DEBUG',
+#        },
+#    },
+#}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
