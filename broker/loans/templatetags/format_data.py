2f9efb047713c419e65c3318fa545b257e91ee96
Lorenzo Tan <lorenzo.tan@gmail.com>
2019-01-20 13:06:42 -0800

    giving credit

--- broker/loans/templatetags/format_data.py
+++ broker/loans/templatetags/format_data.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+# https://wsvincent.com/format-phone-numbers-in-django/
 @register.filter(name='format_phone')
 def format_phone(value):
     return '(' + value[0:3] + ')' + ' ' + value[3:6] + '-' + value[6:10]
