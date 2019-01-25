f191176d6da0d3c48d46a2a64b1a1ce835098f92
Lorenzo Tan <lorenzo.tan@gmail.com>
2019-01-25 04:59:49 -0800

    18: fixed welcome message on homepage

--- broker/home/views.py
+++ broker/home/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
 from django.template import loader
 
 # Create your views here.
 def index(req):
-    #template = loader.get_template('home/index.html')
-    return render_to_response('home/index.html')
+    tmpl = loader.get_template('home/index.html')
+
+    attr = {}
+
+    return HttpResponse(tmpl.render(attr, req))
 
 def denied(req):
     #template = loader.get_template('home/index.html')
