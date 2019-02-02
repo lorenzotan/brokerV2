85de49124cc74ad4a3c898e9407875c12ddc7a74
Lorenzo Tan <lorenzo.tan@gmail.com>
2019-02-02 12:25:24 -0800

    20: fixed logout bug and redirect login to group select if no group assigned

--- broker/accounts/views.py
+++ broker/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group
+from django.contrib.auth import logout
+from django.contrib import auth
 from django.http import HttpResponse
 from django.template import loader
 from django.urls import reverse_lazy
@@ -9,10 +11,14 @@ from .forms import GroupForm
 
 # Create your views here.
 
+# https://overiq.com/django-1-11/django-logging-users-in-and-out/
 def logout(req):
+    auth.logout(req)
     return render_to_response('accounts/logout.html')
 
 # https://stackoverflow.com/questions/51250903/how-to-assign-group-to-user-from-a-registration-form-for-user-in-django
+# if logged in user is not assigned to a group,
+# force user to select a group then redirect to corresponding Lender/Broker/Client form
 def select_group(req):
     tmpl = loader.get_template('accounts/select_group.html')
     if req.method == 'POST':
@@ -26,12 +32,10 @@ def select_group(req):
 
             group_form = 'loans:' + group.name + '_form'
 
-            print ("REDIRECTING TO " + group_form)
             return redirect(group_form)
 
     else:
         form = GroupForm()
-
 
     context = {
         'form': form
--- broker/home/views.py
+++ broker/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, redirect
 from django.http import HttpResponse
 from django.template import loader
 
@@ -12,6 +12,11 @@ def index(req):
     tmpl = loader.get_template('home/index.html')
 
     attr = {}
+
+    user_groups = req.user.groups.all()
+
+    if not user_groups.exists() and req.user.is_authenticated:
+        return redirect('accounts:select_group')
 
     return HttpResponse(tmpl.render(attr, req))
 
